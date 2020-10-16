b8f8b557c9fe2c3926bb04898aa55f7492a90ecd
Brandon True <brandon.true@pcc.edu>
2020-10-15 21:51:53 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by brandon.true on 10/15/2020.
  */
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
