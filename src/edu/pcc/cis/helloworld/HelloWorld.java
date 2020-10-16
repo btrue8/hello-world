f617ee47e8006a28ac4a21b8c6437965368168e7
Brandon True <brandon.true@pcc.edu>
2020-10-15 21:47:04 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by brandon.true on 10/15/2020.
  */
 public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
